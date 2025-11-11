efcecfe41837bdf9a0f76f18faa64373ecf43833
ZettaBite4031 <ddayboy123@gmail.com>
2025-11-11 17:39:46 -0500

    Added a logger for debugging

---
+++ include/netcore/logging_http_transport.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "http_transport.hpp"
+#include <boost/asio/any_io_executor.hpp>
+
+namespace NetCore {
+
+    class LoggingHttpTransport : public IHttpTransport {
+    public: 
+        explicit LoggingHttpTransport(std::shared_ptr<IHttpTransport> inner);
+
+        std::expected<HttpResponse, std::error_code> send_request(const HttpRequest& request) override;
+
+    private:
+        std::shared_ptr<IHttpTransport> m_Inner;
+    };
+
+} // namespace NetCore
---
+++ src/logging_http_transport.cpp
@@ -0,0 +1,22 @@
+#include "netcore/logging_http_transport.hpp"
+
+#include <type_traits>
+#include <print>
+
+namespace NetCore {
+
+    LoggingHttpTransport::LoggingHttpTransport(std::shared_ptr<IHttpTransport> inner) 
+        : m_Inner(std::move(inner)) {
+        static_assert(!std::is_same<LoggingHttpTransport, std::decay_t<decltype(*inner)>>::value,
+                      "LoggingHttpTransports cannot be wrapped inside another!");
+    }
+
+    std::expected<HttpResponse, std::error_code> LoggingHttpTransport::send_request(const HttpRequest& request) {
+        std::println("[HTTP] {} {}", request.method, request.url);
+        auto res = m_Inner->send_request(request);
+        if (res) std::println("[HTTP] -> {}", res->status);
+        else std::println("[HTTP] ERROR: {}", res.error().message());
+        return res;
+    }
+
+} // namespace Netcore
